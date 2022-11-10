ef22c9674fc4540be5df2e3b33fafd06387f3146
Jojo <jojothomas1515@gmail.com>
2022-11-10 11:55:40 +0100

    api table

---
+++ app/db/models/api.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+from sqlalchemy import UniqueConstraint
+from sqlmodel import Field, Relationship
+
+from .common import UUIDModel, TimestampModel
+
+
+def get_random_string(length):
+    """
+    create a random alphaNumeric string to be used as the apikey
+    @param length: used to specify the  length of the required
+    alphaNum string
+    """
+    letters = string.ascii_lowercase
+    numbers = string.digits
+    alphaNum = letters + numbers
+    result_str = ''.join(random.choice(alphaNum) for i in range(length))
+    return result_str
+
+
+class ApiKeys(UUIDModel, TimestampModel, table=True):
+    __table_args__ = (UniqueConstraint("email"),)
+    key: str = Field(default=get_random_string(30))
+
+    history: list['History'] = Relationship(back_populates="history")

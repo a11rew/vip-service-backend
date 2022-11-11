0298e3f813ec2cf3386f828734cf6975b4fed7a5
olatomide007 <olatomideafolabi@gmail.com>
2022-11-11 09:38:51 +0100

    Endpoint Signup first pull request for any corrections

--- app/api/router.py
+++ app/api/router.py
@@ -2,7 +2,10 @@ from fastapi.routing import APIRouter
 
 from api.example.views import router as example_router
 from api.system.views import router as system_router
+from api.signup.views import router as signup_router
 
 api_router = APIRouter()
 api_router.include_router(system_router, prefix="/system", tags=["system"])
 api_router.include_router(example_router, prefix="/example", tags=["example"])
+api_router.include_router(signup_router, prefix="/signup", tags=["signup"])
+
---
+++ app/api/signup/schemas.py
@@ -0,0 +1,21 @@
+# from datetime import datetime
+# from typing import Optional
+# from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class Signup(BaseModel):
+    # id: Optional[UUID] = None
+    first_name: str
+    last_name: int
+    email: str
+    password: str
+    confirmed_password: str
+    # class Config:
+    #     orm_mode = True
+
+
+# class ExampleSchema(ExampleCreateSchema):
+#     created_at: Optional[datetime]
+#     updated_at: Optional[datetime]

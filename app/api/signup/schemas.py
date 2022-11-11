ea5edec4007eaa374508094930973b43667b8a20
olatomide007 <olatomideafolabi@gmail.com>
2022-11-11 22:07:06 +0100

    making necessary for changes: from get to post

--- app/api/signup/schemas.py
+++ app/api/signup/schemas.py
@@ -1,21 +1,8 @@
-# from datetime import datetime
-# from typing import Optional
-# from uuid import UUID
-
 from pydantic import BaseModel
 
 
 class Signup(BaseModel):
-    # id: Optional[UUID] = None
     first_name: str
     last_name: int
     email: str
     password: str
-    
-    # class Config:
-    #     orm_mode = True
-
-
-# class ExampleSchema(ExampleCreateSchema):
-#     created_at: Optional[datetime]
-#     updated_at: Optional[datetime]
--- app/api/signup/views.py
+++ app/api/signup/views.py
@@ -1,14 +1,10 @@
 from fastapi import APIRouter
 from .schemas import Signup
 
+
 router = APIRouter()
 
-@router.get("/", response_model= Signup)
-async def signup_user(first_name: str,last_name: int, email: str, password: str):
-                    show = {
-                        "first_name": first_name,
-                        "last_name": last_name,
-                        "email": email,
-                        "password": password
-                    }
-                    return show
+
+@router.post("/", response_model= Signup)
+async def create_signup(signup: Signup):
+    return signup

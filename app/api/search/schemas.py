c872a08a9c3053571a6bb913b1ff9d9e2fcdeee3
Andrew Glago <andrewglago1@gmail.com>
2022-11-12 23:28:58 +0000

    refactor: update search response

--- app/api/search/schemas.py
+++ app/api/search/schemas.py
@@ -3,8 +3,11 @@ from typing import Optional
 
 
 class SearchResponseSchema(BaseModel):
-    is_vip: bool
     name: str
     gender: Optional[str] = None
     occupation: Optional[str] = None
     age: Optional[int] = None
+    is_vip: Optional[bool] = None
+    vip_score: Optional[int] = None
+    created_at: Optional[str] = None
+    updated_at: Optional[str] = None

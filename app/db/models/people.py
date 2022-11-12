ea95090218d5a89bf0f8d7632e8f339d090b2639
Andrew Glago <andrewglago1@gmail.com>
2022-11-12 23:27:20 +0000

    refactor: update vip to people

--- app/db/models/people.py
+++ app/db/models/people.py
@@ -1,13 +1,15 @@
 from db.models.common import TimestampModel, UUIDModel
 
 
-class Vip(TimestampModel, UUIDModel, table=True):
-    __tablename__ = "vips"
+class People(TimestampModel, UUIDModel, table=True):
+    __tablename__ = "people"
 
     name: str
     age: int
     gender: str
     occupation: str
+    vip_score: int
+    is_vip: bool
 
     def __repr__(self):
         return f"<Vip (id: {self.id})>"

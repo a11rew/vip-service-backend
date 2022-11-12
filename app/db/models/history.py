9aa6a4b5aeab4f60503d9d1b6b4c4288bfc853fe
Andrew Glago <andrewglago1@gmail.com>
2022-11-12 23:39:59 +0000

    refactor: update model fields

--- app/db/models/history.py
+++ app/db/models/history.py
@@ -1,21 +1,16 @@
-from typing import Optional, List
-from sqlmodel import Field, Relationship, JSON, Column
+from sqlmodel import Field, JSON, Column
 from .common import TimestampModel, UUIDModel
-from .user import User
 
-class History(TimestampModel, UUIDModel, table= True):
+
+class History(TimestampModel, UUIDModel, table=True):
     __tablename__ = "history"
-    
-    input: str
-    result: List[str] = Field(sa_column=Column(JSON))
-    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
 
-    user: Optional[User] = Relationship(back_populates="user")
+    input: str = Field(sa_column=Column(JSON))
+    result: int = Field(foreign_key="people.id")
+    user: int = Field(foreign_key="user.id")
 
     class Config:
         arbitrary_types_allowed = True
-        
+
     def __repr__(self):
         return f"<History (id: {self.id})>"
-
-    
